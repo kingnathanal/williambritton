05d1a645af017e6f916c0c4acb1828a3ec33b9ee
William Britton <kingnathanal@gmail.com>
2026-08-14 23:21:00 -0400

    Serve Quiet Systems from homepage

--- src/components/ContactPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <a href="">Photography</a>
-    <a href="">Github</a>
-    <a href="">LinkedIn</a>
-    <a href="">Instagram</a>
-    <a href="">Email</a>
-  </div>
-</template>
